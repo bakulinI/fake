a239e3433095951e659ceef52851458facbd0f62
SlaveRRR <slava.arhipov40020141@gmail.com>
2024-10-15 20:13:35 +0300

    second commit

--- products/views.py
+++ products/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'products/index.html')
+
+def users(request):
+    return render(request, 'products/index.html')
